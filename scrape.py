818f2c27dec0c9102d58fb74125a703c43abca69
fellakmoh876 <mohammedfellak560@gmail.com>
2019-10-16 02:03:46 -0400

    make a regex expression to extract emails

--- scrape.py
+++ scrape.py
@@ -9,8 +9,12 @@ from googlesearch import search
 
 logging.getLogger('scrapy').propagate = False
 
+# extract websites from google with googlesearch
 def get_urls(tag, n, language):
     urls = [url for url in search(tag, stop=n, lang=language)][:n]
     return urls
 
 get_urls('movie rating', 5, 'en')
+
+# make a regex expression to extract emails
+mail_list = re.findall('\w+@\w+\.{1}\w+', html_text)
